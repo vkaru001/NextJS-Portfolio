839d7faffd927f1615bc38e64b7aaf24b474c919
vkaru001 <vkaru001@gmail.com>
2024-05-17 16:32:12 -0500

    skill update

--- src/app/components/ExperienceSection.jsx
+++ src/app/components/ExperienceSection.jsx
@@ -10,22 +10,12 @@ const TAB_DATA = [
     period:"(Jan 2020 - July 2022)",
     content: (
       <ul className="list-disc pl-2">
-
-•	
-•	
-•	
-•	•	Developed multiple proof-of-concepts (POCs) by building various login portals and Product UI builder screens for “HIMS” web applications using HTML, CSS3, JavaScript, and React.
-•	Implemented new UI features and components enhancing user engagement and boosting user retention by 15% on release management.
-•	Initiated CI/CD (Continuous Integration / Continuous Deployment) pipelines using Jenkins that reduced deployment time by 30%, leading to a more efficient software deployment process.
-
-
-
-        <li>Developed internal portals for Hospital's "HIMS" and "LIS" websites' UI with ReactJS, JavaScript and Python, and leveraged AWS services (EC2, S3, RDS) for hosting and managing web applications, ensuring scalability and reliability. </li>
+        <li>Developed internal portals for Hospitals HIMS and LIS websites UI with ReactJS, JavaScript and Python, and leveraged AWS services like, EC2, S3, RDS for hosting and managing web applications, ensuring scalability and reliability. </li>
         <li>Implemented OAuth 2.0 authentication flow using AWS Cognito User Pools, enabling secure user login and access to third-party APIs. </li>
         <li>Monitored and Optimized REST API performance, led to 20% spike in API speed & 30% lower API calls using Redis cache and MongoDB.</li>
         <li>Achieved 30% reduction in website load times through performance optimization techniques, best user experience via enhancing usability. </li>
         <li>Created dynamic widgets to improve doctor availability, reducing appointment booking time by 40%. </li>
-        <li>Conducted code debugging, resolved many JIRA issues, and focused on cross-browser compatibility, unit testing and integration testing and application support.</li>
+        <li>Conducted code debugging, resolved many JIRA issues, and focused on cross-browser compatibility, unit testing, and application support.</li>
       </ul>
     ),
   },
@@ -35,9 +25,9 @@ const TAB_DATA = [
     period:"(Jun 2019 - Dec 2019)",
     content: (
       <ul className="list-disc pl-2">
-        <li>Developed multiple proof-of-concepts (POCs) by building various login portals and Product UI builder screens for "HIMS" web applications using HTML, CSS3, JavaScript, and React.</li>
+        <li>Developed multiple POCs by building various login portals and Product UI screens for HIMS web applications using HTML, CSS3, JavaScript, and React.</li>
         <li>Implemented new UI features and components enhancing user engagement and boosting user retention by 15% on release management.</li>
-        <li>Initiated CI/CD pipelines using Jenkins and Github Actions that reduced deployment time by 30%, ensuring efficient software deployment.</li>
+        <li>Initiated CI CD pipelines using Jenkins and Github Actions that reduced deployment time by 30%, ensuring efficient software deployment.</li>
       </ul>
     ),
   },
